975e062db6149f95969b5c3563b395034c6c32af
oakbr <oak.br@outlook.com>
2025-10-15 09:01:26 -0300

    Atualização em 15-10-2025

--- src/server.ts
+++ src/server.ts
@@ -4,18 +4,22 @@ import mustache from 'mustache-express';
 import path from 'path';
 import mainRoutes from './routes/index'; //Importando a rota principal da home.
 
-//Pega informação do arquivo .env e disponibiliza para todo o projeto. Por exemplo PORT=4000
+//Pega informação do arquivo .env e disponibiliza para todo o projeto. Por exemplo PORT=3000. Arquivo de ser criado na raiz do projeto
 dotenv.config();
 
+//Criar o servidor
 const server = express();
 
+//Definir o mustache no servidor (engine)
 server.set('view engine', 'mustache');
 
+//Definir o diretório publico com as imagens 
 server.set('views', path.join(__dirname, 'views'));
 
+//ativar o engine do servidor
 server.engine('mustache', mustache());
 
-//Configurar pasta publica
+//Configurar pasta publica 
 server.use(express.static(path.join(__dirname, '../public')));
 
 //Rotas
